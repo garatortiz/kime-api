b29955656d8fea097af77703f8e3bcdedaf8bae9
Matias Garat Ortiz <matiasgaratortiz@gmail.com>
2024-10-05 10:02:55 -0300

    fix: coderabbit recommendations

--- src/config/configurations.ts
+++ src/config/configurations.ts
@@ -1,7 +1,11 @@
 export default () => ({
-  port: Number.parseInt(process.env.PORT) || 3000,
+  port: Number.isNaN(Number.parseInt(process.env.PORT))
+    ? 3000
+    : Number.parseInt(process.env.PORT),
   database: {
     host: process.env.DATABASE_HOST,
-    port: Number.parseInt(process.env.DATABASE_PORT) || 5432,
+    port: Number.isNaN(Number.parseInt(process.env.DATABASE_PORT))
+      ? 5432
+      : Number.parseInt(process.env.DATABASE_PORT),
   },
 });
